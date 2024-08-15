4ec465787e3d8d2019a245ceb5fb439ef0ea4f0b
Lukas Bauza <lukasbauza.lb@proton.me>
2024-08-15 10:19:54 +0100

    telescope and treesitter added to neovim

---
+++ .config/nvim/lua/lukas/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        { '<leader>ff', require('telescope.builtin').find_files, desc = '[f]ind [f]iles' },
+        { '<leader>fg', require('telescope.builtin').live_grep, desc = '[f]ile [g]rep' },
+    }
+}
