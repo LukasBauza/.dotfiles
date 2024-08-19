4315c3b3f64031b80dc7aca5bbb834b3f8c58faa
Lukas Bauza <lukasbauza.lb@proton.me>
2024-08-19 13:56:54 +0100

    lsp configuration for nvim

--- .config/nvim/lua/lukas/plugins/lsp.lua
+++ .config/nvim/lua/lukas/plugins/lsp.lua
@@ -8,15 +8,40 @@ return {
         opts = {
             ensure_installed = {
                 "lua_ls",
+                "markdown_oxide",
+                "clangd",
+                "bashls",
+                "cmake",
+                "cssls",
+                "html",
+                "tsserver",
+                "pyright",
+                "ruff",
             },
         },
     },
     {
        "neovim/nvim-lspconfig",
+       lazy = false,
         config = function()
+            local keymap = vim.keymap.set
             require("lspconfig").lua_ls.setup({})
+            require("lspconfig").markdown_oxide.setup({})
+            require("lspconfig").clangd.setup({})
+            require("lspconfig").bashls.setup({})
+            require("lspconfig").cmake.setup({})
+            require("lspconfig").cssls.setup({})
+            require("lspconfig").html.setup({})
+            require("lspconfig").tsserver.setup({})
+            require("lspconfig").pyright.setup({})
+            require("lspconfig").ruff.setup({})
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            -- TODO: Add desc, for keymaps.
+            -- TODO: Recreate better potential keymaps.
+            keymap('n', 'gd', vim.lsp.buf.definition, {})
+            keymap('n', 'K', vim.lsp.buf.hover, {})
+            keymap('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            keymap('n', '<leader>rn', vim.lsp.buf.rename, {})
         end,
     }
 }
--- .config/nvim/lua/lukas/plugins/telescope.lua
+++ .config/nvim/lua/lukas/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim', branch = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = false,
     opts = {},
     keys = {
         { '<leader>ff', require('telescope.builtin').find_files, desc = '[f]ind [f]iles' },
