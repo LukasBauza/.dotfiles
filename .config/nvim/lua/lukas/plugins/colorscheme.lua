36cda148cd7c0bc97c910f4bf06804b8f3f5b492
Lukas Bauza <lukasbauza.lb@proton.me>
2024-08-27 11:17:41 +0100

    Remove gitter background kanagawa

--- .config/nvim/lua/lukas/plugins/colorscheme.lua
+++ .config/nvim/lua/lukas/plugins/colorscheme.lua
@@ -2,6 +2,18 @@ return {
   "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
-        vim.cmd('colorscheme kanagawa-wave')
+        require("kanagawa").setup({
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none"
+                        }
+                    }
+                }
+            }
+        })
+
+        vim.cmd("colorscheme kanagawa-wave")
   end
 }
