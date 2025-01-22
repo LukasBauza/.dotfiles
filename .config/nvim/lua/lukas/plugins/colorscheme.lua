3c1a17e7739d47061799a3f711516ddb17979fe3
Lukas Bauza <lukasbauza.lb@proton.me>
2025-01-22 12:08:24 +0000

    moonfly colorscheme added for neovim

--- .config/nvim/lua/lukas/plugins/colorscheme.lua
+++ .config/nvim/lua/lukas/plugins/colorscheme.lua
@@ -1,19 +1,30 @@
-return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
-  config = function()
-        require("kanagawa").setup({
-            colors = {
-                theme = {
-                    all = {
-                        ui = {
-                            bg_gutter = "none"
-                        }
-                    }
-                }
-            }
-        })
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   priority = 1000,
+--   config = function()
+--         require("kanagawa").setup({
+--             colors = {
+--                 theme = {
+--                     all = {
+--                         ui = {
+--                             bg_gutter = "none"
+--                         }
+--                     }
+--                 }
+--             }
+--         })
+--
+--         vim.cmd("colorscheme kanagawa-wave")
+--   end
+-- }
+--
 
-        vim.cmd("colorscheme kanagawa-wave")
-  end
+return {
+    "bluz71/vim-moonfly-colors",
+    name = "moonfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        vim.cmd("colorscheme moonfly")
+    end
 }
