3224f8b1b848c00d37ba47140c3c2ba6cbb26396
Lukas Bauza <lukasbauza.lb@proton.me>
2025-01-22 12:10:48 +0000

    moonfly theme for wezterm added

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 -- My configurations
-config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "Moonfly (Gogh)"
 
 config.hide_tab_bar_if_only_one_tab = true
 
